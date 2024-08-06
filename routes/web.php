55f1bcf037e2ae680d54b17c9c12ca69e99b0829
yousuke takahashi <y.takahashi@necomata.co.jp>
2024-08-06 18:25:39 +0900

    laravelのサンプルプロジェクトを登録

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/dashboard', [BookController::class, 'index']);
+Route::get('/shopping-cart', [BookController::class, 'bookCart'])->name('shopping.cart');
+Route::get('book/{id}', [BookController::class, 'addBooktoCart'])->name('addbook.to.cart');
+Route::patch('/update-shopping-cart', [BookController::class, 'updateCart'])->name('update.shopping.cart');
+Route::delete('/delete-cart-product', [BookController::class, 'deleteProduct'])->name('delete.cart.product');
