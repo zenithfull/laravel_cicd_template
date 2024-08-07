c8f8aaf936c0753560b4495894ff9a764365706c
yousuke takahashi <y.takahashi@necomata.co.jp>
2024-08-07 11:48:41 +0900

    静的解析エラーのテスト

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -11,6 +11,7 @@ class BookController extends Controller
     {
         $books = Book::All();
         return view('products', compact('books'));
+        $books = null;
     }
 
     public function bookCart()
@@ -23,8 +24,7 @@ class BookController extends Controller
         $book = Book::findOrFail($id);
         $cart = session()->get('cart', []);
 
-        if(isset($cart[$id]))
-        {
+        if (isset($cart[$id])) {
             $cart[$id]['quantity']++;
         } else {
             $cart[$id] = [
@@ -41,8 +41,7 @@ class BookController extends Controller
 
     public function updateCart(Request $request)
     {
-        if($request->id && $request->quantity)
-        {
+        if ($request->id && $request->quantity) {
             $cart = session()->get('cart');
             $cart[$request->id]["quantity"] = $request->quantity;
             session()->put('cart', $cart);
@@ -52,11 +51,9 @@ class BookController extends Controller
 
     public function deleteProduct(Request $request)
     {
-        if($request->id)
-        {
+        if ($request->id) {
             $cart = session()->get('cart');
-            if(isset($cart[$request->id]))
-            {
+            if (isset($cart[$request->id])) {
                 unset($cart[$request->id]);
                 session()->put('cart', $cart);
             }
