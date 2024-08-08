b28db87709fce19615bc377f0bb9101b3b3086ba
yousuke takahashi <y.takahashi@necomata.co.jp>
2024-08-08 11:05:51 +0900

    Unitテストでエラーになるケースを追加

--- tests/Unit/BookTest.php
+++ tests/Unit/BookTest.php
@@ -20,4 +20,12 @@ class BookTest extends TestCase
 
         $this->assertEquals($fillable, $book->getFillable());
     }
+
+    public function test_book_fillable_attributes_error_case(): void
+    {
+        $fillable = ['name', 'author', 'image', 'price', 'seller'];
+        $book = new Book();
+
+        $this->assertEquals($fillable, $book->getFillable());
+    }
 }
